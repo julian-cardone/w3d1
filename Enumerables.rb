a1c6b1759e4a3b227ff085c4c392058a411b37c9
rtexelm <texelist@gmail.com>
2022-09-26 18:04:24 -0400

    Didn't really get far here

--- Enumerables.rb
+++ Enumerables.rb
@@ -53,7 +53,7 @@ class Array
             if !r.is_a? Array
                 flatten << r
             else
-            flatten += [r.my_flatten]
+                r.my_flatten
             end
         end
         return flatten
@@ -70,7 +70,7 @@ end
 # p a.my_reject { |num| num > 1 } # => [1]
 # p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
-a = [1, 2, 3]
+# a = [1, 2, 3]
 # p a.my_any? { |num| num > 1 } # => true
 # p a.my_any? { |num| num == 4 } # => false
 # p a.my_all? { |num| num > 1 } # => false
